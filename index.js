cc02f69a921fcb482e8ec217929fdf60e02708cf
Crystal Korth <crystalkorth@gmail.com>
2022-06-01 20:18:28 -0500

    added red character

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-function newImage(url, left, bottom){
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
+
+
+function move(image){
+    image.style.position = 'fixed'
+    
+    function moveToCoordinates(left, bottom){
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
+
+    return {
+        to: moveToCoordinates
+    }
+}
+
+move(newImage('assets/green-character.gif')).to(100, 250)
+move(newImage('assets/red-character.gif')).to(10, 250)
+
+
+
 
 newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
